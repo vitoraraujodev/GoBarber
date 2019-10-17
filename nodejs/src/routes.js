91f5296a96ea012fc3b2b91f90de46bb37ed607b
vitoraraujo <vitoraraujo.dev@gmail.com>
2019-10-17 19:47:52 -0300

    Criando loader de models

--- nodejs/src/routes.js
+++ nodejs/src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', ( req, res ) => {
-  return res.json({ message: 'Hello World' });
+routes.get('/', async ( req, res ) => {
+  const user = await User.create({
+    name: 'Vitor Araujo',
+    email: 'vitoraraujo7@hotmail.com',
+    password_hash: '111111'
+  })
+  return res.json(user);
 });
 
 export default routes;
