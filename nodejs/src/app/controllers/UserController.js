f387d27a4c4d7a5388f8ffc33af309e56e999ea7
vitoraraujo <vitoraraujo.dev@gmail.com>
2019-10-19 17:31:56 -0300

    Update de usuário

--- nodejs/src/app/controllers/UserController.js
+++ nodejs/src/app/controllers/UserController.js
@@ -19,11 +19,34 @@ class UserController {
   }
 
   async update(req, res) {
-    console.log(req.userId);
+    const { email, oldPassword } = req.body;
 
-    // const user = User.findByPk(req.userId);
+    const user = await User.findByPk(req.userId);
 
-    return res.json({ ok: true });
+    if (email !== user.email) {
+      const userExists = await User.findOne({
+        where: { email },
+      });
+
+      if (userExists) {
+        return res.status(400).json({ errer: 'User already exists.' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Old Password does not match.' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({
+      user: {
+        id,
+        name,
+        email,
+        provider,
+      },
+    });
   }
 }
 
