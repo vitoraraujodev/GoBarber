97b8f7acee02208606fa2576584ec3ae96311398
vitoraraujo <vitoraraujo.dev@gmail.com>
2019-10-19 15:35:38 -0300

    Cadastro de usuários

--- nodejs/src/routes.js
+++ nodejs/src/routes.js
@@ -1,15 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async ( req, res ) => {
-  const user = await User.create({
-    name: 'Vitor Araujo',
-    email: 'vitoraraujo7@hotmail.com',
-    password_hash: '111111'
-  })
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
