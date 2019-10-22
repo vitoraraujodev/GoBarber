e9c7771b5f819ae3d66c26b151685e057b5af0de
vitoraraujo <vitoraraujo.dev@gmail.com>
2019-10-22 19:36:24 -0300

    Listando agenda do prestador

--- nodejs/src/routes.js
+++ nodejs/src/routes.js
@@ -7,6 +7,7 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -22,6 +23,8 @@ routes.get('/providers', ProviderController.index);
 
 routes.get('/appointments', authMiddleware, AppointmentController.index);
 routes.post('/appointments', authMiddleware, AppointmentController.store);
+
+routes.get('/schedule', authMiddleware, ScheduleController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 export default routes;
