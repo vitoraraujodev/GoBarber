49fc27e66538d248bbc7fa8509c3209ca9621136
vitoraraujo <vitoraraujo.dev@gmail.com>
2019-10-22 14:03:50 -0300

    Listagem de prestadores de serviço

--- nodejs/src/routes.js
+++ nodejs/src/routes.js
@@ -5,6 +5,7 @@ import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -15,6 +16,8 @@ routes.post('/users', UserController.store);
 routes.put('/users', authMiddleware, UserController.update);
 
 routes.post('/sessions', SessionController.store);
+
+routes.get('/providers', ProviderController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 export default routes;
