f7918e96c7d443b4073e18a036d4166e712e1eaf
vitoraraujodev <vitoraraujo.dev@gmail.com>
2019-12-15 11:59:47 -0300

    Utilizando Root import

--- reactjs/gobarber/src/routes/index.js
+++ reactjs/gobarber/src/routes/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
 
-import Route from './Route'
+import Route from './Route';
 
-import SignIn from '../pages/SignIn';
-import SignUp from '../pages/SignUp';
-import Dashboard from '../pages/Dashboard';
-import Profile from '../pages/Profile';
+import SignIn from '~/pages/SignIn';
+import SignUp from '~/pages/SignUp';
+import Dashboard from '~/pages/Dashboard';
+import Profile from '~/pages/Profile';
 
 export default function Routes() {
   return (
@@ -15,7 +15,6 @@ export default function Routes() {
       <Route path="/register" component={SignUp} />
       <Route path="/dashboard" component={Dashboard} isPrivate />
       <Route path="/profile" component={Profile} isPrivate />
-
     </Switch>
   );
 }
