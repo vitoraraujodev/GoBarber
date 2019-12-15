6f9c68343472eb8237c95504c1d2d640e2c9af0c
vitoraraujodev <vitoraraujo.dev@gmail.com>
2019-12-15 14:21:43 -0300

    Validações

--- reactjs/gobarber/src/pages/SignUp/index.js
+++ reactjs/gobarber/src/pages/SignUp/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('Nome é obrigatório'),
+  email: Yup.string()
+    .email('E-mail inválido')
+    .required('E-mail é obrigatório'),
+  password: Yup.string()
+    .min(6, 'Mínimo de 6 caractefres')
+    .required('Senha é obrigatória'),
+});
 
 export default function SignUp() {
   function handleSubmit(data) {
@@ -14,7 +24,7 @@ export default function SignUp() {
     <>
       <img src={logo} alt="GoBarber" />
 
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="name" placeholder="Nome completo" />
         <Input name="email" type="email" placeholder="Seu e-email" />
         <Input name="password" type="password" placeholder="Seu senha" />
