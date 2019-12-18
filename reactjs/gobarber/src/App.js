f608981aa65176e8103987665e387a86dea46c2e
vitoraraujodev <vitoraraujo.dev@gmail.com>
2019-12-18 12:25:31 -0300

    Salvando dados locais do usuario

--- reactjs/gobarber/src/App.js
+++ reactjs/gobarber/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 import history from './services/history';
@@ -6,17 +7,19 @@ import history from './services/history';
 import './config/ReactotronConfig';
 import Routes from './routes';
 
-import store from './store';
+import { store, persistor } from './store';
 
 import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <Provider store={store}>
-      <Router history={history}>
-        <Routes />
-        <GlobalStyle />
-      </Router>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes />
+          <GlobalStyle />
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
