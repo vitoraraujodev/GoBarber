2dfbcd280ee8ad23a14c70d4d44fdfb7c3034f9b
vitoraraujodev <vitoraraujo.dev@gmail.com>
2019-12-18 14:58:09 -0300

    Requisições autenticadas

--- reactjs/gobarber/src/store/modules/auth/sagas.js
+++ reactjs/gobarber/src/store/modules/auth/sagas.js
@@ -29,4 +29,32 @@ export function* signIn({ payload }) {
   }
 }
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
+export function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
+
+    yield call(api.post, 'users', { name, email, password, provider: true });
+
+    history.push('/');
+  } catch (err) {
+    toast.error('Falha no cadastro. Verifique seus dados');
+
+    yield put(signFailure());
+  }
+}
+
+export function setToken({ payload }) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
+
+export default all([
+  takeLatest('persist/REHYDRATE', setToken),
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+]);
