4dabb31db0b545ecb957bd501d1a84263b33249b
vitoraraujodev <vitoraraujo.dev@gmail.com>
2019-12-18 12:14:49 -0300

    Redux para profile de usuario

---
+++ reactjs/gobarber/src/store/modules/user/reducer.js
@@ -0,0 +1,16 @@
+import producer from 'immer';
+
+const INITIAL_STATE = {
+  profile: null,
+};
+
+export default function user(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    case '@auth/SIGN_IN_SUCCESS':
+      return producer(state, draft => {
+        draft.profile = action.payload.user;
+      });
+    default:
+      return state;
+  }
+}
