3f3dd091af427e08322302593131dee4f3e87d87
vitoraraujodev <vitoraraujo.dev@gmail.com>
2020-01-04 15:55:42 -0300

    Página SignIn

--- mobile/gobarber/src/pages/SignIn/index.js
+++ mobile/gobarber/src/pages/SignIn/index.js
@@ -1,17 +1,42 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image } from 'react-native';
+
+import logo from '~/assets/logo.png';
 
 import Background from '~/components/Background';
-import Input from '~/components/Input';
-import Button from '~/components/Button';
 
-import { Container } from './styles';
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
 
 export default function SignIn() {
   return (
     <Background>
       <Container>
-        <Image />
+        <Image source={logo} />
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu email"
+          />
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Digite sua senha"
+          />
+          <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+        </Form>
+        <SignLink onPress={() => {}}>
+          <SignLinkText>Criar conta agora</SignLinkText>
+        </SignLink>
       </Container>
     </Background>
   );
