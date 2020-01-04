ff7df58eccae9476dcc28f1f65a91a1cf210f513
vitoraraujodev <vitoraraujo.dev@gmail.com>
2020-01-04 15:38:14 -0300

    Input & Button

--- mobile/gobarber/src/pages/SignIn/index.js
+++ mobile/gobarber/src/pages/SignIn/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { Text } from 'react-native';
 
 import Background from '~/components/Background';
+import Input from '~/components/Input';
+import Button from '~/components/Button';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function SignIn() {
   return (
     <Background>
-      <Text>SignIn</Text>
+      <Container>
+        <Image />
+      </Container>
     </Background>
   );
 }
