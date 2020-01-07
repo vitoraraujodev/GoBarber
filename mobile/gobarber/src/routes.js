d52e314e1f83ea6bac0e14a57dc91d19811ba597
vitoraraujodev <vitoraraujo.dev@gmail.com>
2020-01-07 00:48:51 -0300

    Rotas de agendamento

--- mobile/gobarber/src/routes.js
+++ mobile/gobarber/src/routes.js
@@ -1,8 +1,17 @@
+import React from 'react';
+
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+
+import SelectProvider from './pages/New/SelectProvider';
+import SelectDateTime from './pages/New/SelectDateTime';
+import Confirm from './pages/New/Confirm';
 
 import Dashboard from './pages/Dashboard';
 import Profile from './pages/Profile';
@@ -13,7 +22,39 @@ export default (isSigned = false) =>
       {
         Sign: createSwitchNavigator({ SignIn, SignUp }),
         App: createBottomTabNavigator(
-          { Dashboard, Profile },
+          {
+            Dashboard,
+            New: {
+              screen: createStackNavigator(
+                {
+                  SelectProvider,
+                  SelectDateTime,
+                  Confirm,
+                },
+                {
+                  defaultNavigationOptions: {
+                    headerTransparent: true,
+                    headerTintColor: '#fff',
+                    headerLeftContainerStyle: {
+                      marginLeft: 20,
+                    },
+                  },
+                }
+              ),
+              navigationOptions: {
+                tabBarVisible: false, // para tirar o footer nesse navigator
+                tabBarLabel: 'Agendar',
+                tabBarIcon: (
+                  <Icon
+                    name="add-circle-outline"
+                    size={20}
+                    color="rgba(255,255,255,0.6)"
+                  />
+                ),
+              },
+            },
+            Profile,
+          },
           {
             tabBarOptions: {
               keyboardHidesTabBar: true,
